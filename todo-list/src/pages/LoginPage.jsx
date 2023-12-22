96bd6531ee0b27c23ca7886500c7ba32b264eba8
bitress <cyannejustinl.vega12@gmail.com>
2023-12-22 21:13:56 +0800

    moved login function to services

--- todo-list/src/pages/LoginPage.jsx
+++ todo-list/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import { Notyf } from 'notyf';
 import 'notyf/notyf.min.css';
 import { setCookie } from "../utils/cookie.js";
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "../config.js";
+import {login} from "../services/authService.jsx";
 
 function LoginPage({ setLoggedIn }) {
 
@@ -22,35 +22,9 @@ function LoginPage({ setLoggedIn }) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = {
-            action: 'login',
-            username: username,
-            password: password
-        };
 
-        try {
-            const response = await fetch(API_URL, {
-                method: 'POST',
-                body: JSON.stringify(data)
-            });
+        await login(username, password, setLoggedIn, navigate, setCookie, notyf);
 
-            const responseData = await response.json();
-
-            if (responseData.success) {
-                setCookie('jwt', responseData.jwt, 1);
-                notyf.success(responseData.message);
-
-                setTimeout(function (){
-                    setLoggedIn(true);
-                    navigate('/');
-                },3000)
-
-            } else {
-                notyf.error(responseData.message);
-            }
-        } catch (error) {
-            console.error('Fetch request failed:', error);
-        }
     };
 
 
--- todo-list/src/services/authService.jsx
+++ todo-list/src/services/authService.jsx
@@ -1,2 +1,32 @@
-import axios from "axios";
+// authServices.js
+import {API_URL} from "../config.js";
+export const login = async (username, password, setLoggedIn, navigate, setCookie, notyf) => {
+    try {
+        const data = {
+            action: 'login',
+            username: username,
+            password: password
+        };
 
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+
+        const responseData = await response.json();
+
+        if (responseData.success) {
+            setCookie('jwt', responseData.jwt, 1);
+            notyf.success(responseData.message);
+
+            setTimeout(function () {
+                setLoggedIn(true);
+                navigate('/');
+            }, 3000);
+        } else {
+            notyf.error(responseData.message);
+        }
+    } catch (error) {
+        console.error('Fetch request failed:', error);
+    }
+};
